845b29b3f38739158ec30427d838b84e0ffaf07c
Anwar <anwarhussaini160@gmail.com>
2023-07-15 09:33:18 +0430

    2. Let's Build a Steps Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+export default function App() {
+  const step = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active': ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active': ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active': ''}`}>3</div>        
+      </div>
+      <p className="message">Step {step}: {messages[step-1]}</p>
+      <div className="buttons">
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
+        
+      </div>
     </div>
   );
 }
-
-export default App;
